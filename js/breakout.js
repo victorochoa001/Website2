bb97ca0cf3d0cb2c6289fd72732fc03758447b69
victorochoa001 <142928042+victorochoa001@users.noreply.github.com>
2024-03-20 17:17:47 +0000

    Wed, Mar 20, 2024, 10:17 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -1,12 +1,43 @@
-rulesBtn = document.getzelementById('rules-btn')
-rulesBtn = document.getzelementById('rules')
-closeBtn = document.getElementById('close-btn')
+show = document.getElementById('rules-btn');
+close = document.getElementById('close-btn');
+rules = document.getElementById('rules');
+canvas = document.getElementById('canvas');
+ctx = canvas.getContext('2d');
+
+ball = {
+    x: canvas.width /2,
+    y: canvas.height /2,
+    size: 10,
+    speed: 4,
+    dx: 4,
+    dy: -4,
+}
 
 
+paddle = {
+    x: canvas.width /2 - 40,
+    y: canvas.height -20,
+    w: 80,
+    h: 10,
+    speed: 8,
+    dx: 0,
+}
 
-rulesBtn.addEventListener('click', () => {
-    rules.classList.add('show')
+function drawBall() {
+    ctx.beginPath()
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2, true)
+    ctx.fillStyle = '#0095DD'
+    ctx.fill()
+    ctx.closePath()
+    ctx.stroke()
+}
+
+drawBall()
+
+show.addEventListener('click', () => {
+    rules.classList.toggle('show')
 })
-closeBtn.addEventListener('click', () => {
-    rules.classList.remove('show')
+
+close.addEventListener('click', () => {
+    rules.classList.toggle('show')
 })
