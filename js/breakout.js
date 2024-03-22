8e312b1b994aa9dcbb32ff4f7a47ef5f59ce2f64
victorochoa001 <142928042+victorochoa001@users.noreply.github.com>
2024-03-22 16:46:29 +0000

    Fri, Mar 22, 2024, 9:46 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -1,12 +1,16 @@
-show = document.getElementById('rules-btn');
+how = document.getElementById('rules-btn');
 close = document.getElementById('close-btn');
 rules = document.getElementById('rules');
 canvas = document.getElementById('canvas');
 ctx = canvas.getContext('2d');
 
+score = 0;
+BrickRowCount = 9;
+BrickColumnCount = 5;
+
 ball = {
-    x: canvas.width /2,
-    y: canvas.height /2,
+    x: canvas.width / 2,
+    y: canvas.height / 2,
     size: 10,
     speed: 4,
     dx: 4,
@@ -15,12 +19,43 @@ ball = {
 
 
 paddle = {
-    x: canvas.width /2 - 40,
-    y: canvas.height -20,
+    x: canvas.width / 2 - 40,
+    y: canvas.height - 20,
     w: 80,
     h: 10,
     speed: 8,
     dx: 0,
+}
+
+BrickInfo = {
+    w: 70,
+    h: 20,
+    padding: 10,
+    offsetX: 45,
+    offsetY: 60,
+    visible: true,
+}
+
+bricks = []
+for (let i = 0; i < BrickRowCount; i++) {
+    bricks[i] = []
+    for (let j = 0; j < BrickColumnCount; j++) {
+        const x = i * (BrickInfo.w + BrickInfo.padding) + BrickInfo.offsetX
+        const y = j * (BrickInfo.h + BrickInfo.padding) + BrickInfo.offsetY
+        bricks[i][j] = {
+            x,
+            y,
+            ...BrickInfo,
+        }
+    }
+}
+
+function drawPaddle() {
+    ctx.beginPath()
+    ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h)
+    ctx.fillStyle = '#0095DD'
+    ctx.fill()
+    ctx.closePath()
 }
 
 function drawBall() {
@@ -32,7 +67,32 @@ function drawBall() {
     ctx.stroke()
 }
 
-drawBall()
+function drawScore() {
+    ctx.font = '20px Arial'
+    ctx.fillText(`Score: ${score}`, canvas.width - 100, 30)
+}
+
+function drawBricks() {
+    bricks.forEach(column => {
+        column.forEach(brick => {
+            ctx.beginPath()
+            ctx.rect(brick.x, brick.y, brick.w, brick.h)
+            ctx.fillStyle = brick.visible ? '#0095dd' : 'transparent'
+            ctx.fill()
+            ctx.closePath()
+        })
+    })
+}
+
+
+function draw() {
+    drawPaddle()
+    drawBall()
+    drawScore()
+    drawBricks()
+}
+
+draw()
 
 show.addEventListener('click', () => {
     rules.classList.toggle('show')
